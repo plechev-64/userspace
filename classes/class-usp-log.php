c2eb827ca6b83a1bc19f173857290a29dec076fd
Preci1 <35058782+Preci1@users.noreply.github.com>
2021-07-17 18:47:58 +0300

    tab -> space

--- classes/class-usp-log.php
+++ classes/class-usp-log.php
@@ -2,45 +2,45 @@
 
 class USP_Log {
 
-	public $log_path;
+    public $log_path;
 
-	function __construct( $args = false ) {
+    function __construct( $args = false ) {
 
-		if ( $args )
-			$this->init_properties( $args );
+        if ( $args )
+            $this->init_properties( $args );
 
-		if ( ! $this->log_path ) {
+        if ( ! $this->log_path ) {
 
-			$logDir = USP_TAKEPATH . 'logs/';
+            $logDir = USP_TAKEPATH . 'logs/';
 
-			if ( ! file_exists( $logDir ) )
-				wp_mkdir_p( $logDir );
+            if ( ! file_exists( $logDir ) )
+                wp_mkdir_p( $logDir );
 
-			$this->log_path = $logDir . date( 'Y-m-d' ) . '.log';
-		}
-	}
+            $this->log_path = $logDir . date( 'Y-m-d' ) . '.log';
+        }
+    }
 
-	function init_properties( $args ) {
-		$properties = get_class_vars( get_class( $this ) );
+    function init_properties( $args ) {
+        $properties = get_class_vars( get_class( $this ) );
 
-		foreach ( $properties as $name => $val ) {
-			if ( isset( $args[$name] ) )
-				$this->$name = $args[$name];
-		}
-	}
+        foreach ( $properties as $name => $val ) {
+            if ( isset( $args[$name] ) )
+                $this->$name = $args[$name];
+        }
+    }
 
-	function insert_title( $title ) {
+    function insert_title( $title ) {
 
-		$this->insert_log( date( 'H:i:s' ) . " " . $title );
-	}
+        $this->insert_log( date( 'H:i:s' ) . " " . $title );
+    }
 
-	function insert_log( $data ) {
+    function insert_log( $data ) {
 
-		if ( ! is_string( $data ) ) {
-			$data = print_r( $data, true );
-		}
+        if ( ! is_string( $data ) ) {
+            $data = print_r( $data, true );
+        }
 
-		file_put_contents( $this->log_path, $data . "\n", FILE_APPEND );
-	}
+        file_put_contents( $this->log_path, $data . "\n", FILE_APPEND );
+    }
 
 }
